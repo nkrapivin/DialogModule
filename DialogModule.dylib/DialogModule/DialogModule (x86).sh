2602bd5dfd166331b79765fdc42251e5238d11bf
time-killer-games <samuelvenable@hotmail.com>
2019-09-25 08:53:12 -0400

    Delete DialogModule (x86).sh

--- DialogModule.dylib/DialogModule/DialogModule (x86).sh
+++
@@ -1,4 +0,0 @@
-cd "${0%/*}"
-g++ -c -std=c++17 "DialogModule.part1.cpp" -fPIC -m32
-g++ -c -ObjC "DialogModule.part2.mm" -fPIC -m32
-g++ "DialogModule.part1.o" "DialogModule.part2.o" -o "DialogModule (x86)/DialogModule.dylib" -shared -framework Cocoa -fPIC -m32
