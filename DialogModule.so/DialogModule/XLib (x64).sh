587f968bf4bd206dc71be14d76211a2a2d2a7e52
time-killer-games <samuelvenable@hotmail.com>
2019-11-26 22:30:27 -0500

    Update XLib (x64).sh

--- DialogModule.so/DialogModule/XLib (x64).sh
+++ DialogModule.so/DialogModule/XLib (x64).sh
@@ -1,3 +1,4 @@
 cd "${0%/*}"
 g++ -c -std=c++17 "GameMaker.cpp" "XLib.cpp" "lodepng.cpp" -fPIC -m64
-g++ "GameMaker.o" "XLib.o" "lodepng.o" -o "DialogModule (x64)/DialogModule.so" -shared -fPIC -m64 -lprocps
+g++ "GameMaker.o" "XLib.o" "lodepng.o" -o "DialogModule (x64)/DialogModule.so" -shared -fPIC -m64 -lprocps   # Linux
+# g++ "GameMaker.o" "XLib.o" "lodepng.o" -o "DialogModule (x64)/DialogModule.so" -shared -fPIC -m64  -lutil  # BSD
