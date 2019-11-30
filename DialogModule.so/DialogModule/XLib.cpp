f7a4650f089197bc3612ad1ef26f96a7835f9677
time-killer-games <samuelvenable@hotmail.com>
2019-11-30 02:10:17 -0500

    Update XLib.cpp

--- DialogModule.so/DialogModule/XLib.cpp
+++ DialogModule.so/DialogModule/XLib.cpp
@@ -268,7 +268,7 @@ void modify_dialog(pid_t ppid) {
   Window window, parent = owner ? (Window)owner : XGetActiveWindow(display);
 
   unsigned i = 0;
-  while (i < 10) {
+  while (i < 100) {
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
     if (IsPpidOfPid(XGetActiveProcessId(display), ppid) == IsPpidOfPid(getpid(), ppid)) {
       window = XGetActiveWindow(display);
