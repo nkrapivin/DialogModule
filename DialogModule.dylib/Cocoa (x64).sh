e8c50d260fb8918880c8d4d2e2a91c195169c1ba
time-killer-games <samuelvenable@hotmail.com>
2020-04-21 18:22:44 -0400

    Delete Cocoa (x64).sh

--- DialogModule.dylib/Cocoa (x64).sh
+++
@@ -1,4 +0,0 @@
-cd "${0%/*}"
-clang++ -c -std=c++17 "GameMaker.cpp" "Cocoa.part1.cpp" -fPIC -m64
-clang++ -c -ObjC "Cocoa.part2.mm" -fPIC -m64
-clang++ "GameMaker.o" "Cocoa.part1.o" "Cocoa.part2.o" -o "DialogModule (x64)/DialogModule.dylib" -shared -framework Cocoa -fPIC -m64
