49b5eed3bdb8723abd083b66c21ff7e0e6e87f22
time-killer-games <samuelvenable@hotmail.com>
2020-05-09 04:18:29 -0400

    Update XLib.cpp

--- DialogModule.so/DialogModule/XLib.cpp
+++ DialogModule.so/DialogModule/XLib.cpp
@@ -446,10 +446,10 @@ int show_message_helperfunc(char *str) {
     add_escaping(str, false, "") + str_icon + str_echo;
   }
   else if (dm_dialogengine == dm_kdialog) {
-    str_cancel = string("--msgbox \"") + add_escaping(str, false, "") + string("\" --ok-label \"" + add_escaping(btn_array[BUTTON_OK], true, "") + " + "\" --icon dialog-information ");
+    str_cancel = string("--msgbox \"") + add_escaping(str, false, "") + string("\" --ok-label \"" + add_escaping(btn_array[BUTTON_OK], true, "") + "\" --icon dialog-information ");
 
     if (message_cancel)
-      str_cancel = string("--yesno \"") + add_escaping(str, false, "") + string("\" --yes-label \"" + add_escaping(btn_array[BUTTON_OK], true, "") + " + "\" --no-label \"" + add_escaping(btn_array[BUTTON_CANCEL], true, "") + "\" --icon dialog-question ");
+      str_cancel = string("--yesno \"") + add_escaping(str, false, "") + string("\" --yes-label \"" + add_escaping(btn_array[BUTTON_OK], true, "") + "\" --no-label \"" + add_escaping(btn_array[BUTTON_CANCEL], true, "") + "\" --icon dialog-question ");
 
     str_command = string("kdialog ") +
     string("--attach=") + window + string(" ") +
